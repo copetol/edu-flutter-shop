102ff1db742634c8baf770d408b91d834b3bd5b3
Kostya <co@MacBook-Pro-Kostya.local>
2023-06-08 11:30:55 +0300

    add side drawer

--- screens/products_overview_screen.dart
+++ screens/products_overview_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
 import '../providers/cart.dart';
 import '../widgets/badge.dart';
+import '../widgets/app_drawer.dart';
 import './cart_screen.dart';
 
 enum FilterOptions {favorites, all}
@@ -56,6 +57,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ]
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites)
     );
   }
